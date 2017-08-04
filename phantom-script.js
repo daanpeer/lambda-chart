9ac2fe5495be312b98fcbfbd4f3c816b562c6cd1
Daan Houweling <daan.gdh@gmail.com>
2017-08-04 23:28:40 +0200

    Added debug statements

--- phantom-script.js
+++ phantom-script.js
@@ -6,6 +6,13 @@ const filename = system.args[1];
 const data = system.args[2];
 const requestData = JSON.parse(atob(data))
 
+
+console.log('retrieved request data', JSON.stringify(requestData));
+
+page.onResourceRequested = function (request) {
+  console.log('Request ' + JSON.stringify(request, undefined, 4));
+};
+
 console.log('requesting page with args', system.args);
 page.open(url, 'post', JSON.stringify({ chartData: requestData }), function(status) {
   page.render('/tmp/' + filename);
